8b1a9417cbcbd3cb0dc97e5d38041bf2341beee1
michael <misasokolov76@gmail.com>
2026-03-01 14:24:16 +0300

    [Init]

---
+++ src/main/java/by/michael/authorization_example/AuthorizationExampleApplication.java
@@ -0,0 +1,11 @@
+package by.michael.authorization_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationExampleApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(AuthorizationExampleApplication.class, args);
+  }
+}
---
+++ src/test/java/by/michael/authorization_example/AuthorizationExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package by.michael.authorization_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
